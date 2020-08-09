57604ac503c111f2bf7a2b53be982a5da3762625
AnuragGarimella <anurag.garimella@mitratech.com>
2020-08-09 12:35:42 +0530

    Installed entity framework on Project1 and also modified Index.cshtml.

--- Project1/Controllers/HomeController.cs
+++ Project1/Controllers/HomeController.cs
@@ -1,8 +1,5 @@
 ﻿using DBClassLib.GenericRepositoryClasses;
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace Project1.Controllers
